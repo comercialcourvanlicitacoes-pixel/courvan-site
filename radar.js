efaab32283e710e691b7c9c19b53dbe9fb376eaa
comercialcourvanlicitacoes-pixel <comercial.courvanlicitacoes@gmail.com>
2026-05-14 14:26:38 -0300

    Create radar.js

---
+++ radar.js
@@ -0,0 +1,25 @@
+async function buscarLicitacoes(){
+
+  try{
+
+    const response = await fetch(
+      "https://pncp.gov.br/api/consulta/v1/contratacoes/publicacao?pagina=1&tamanhoPagina=20"
+    );
+
+    const data = await response.json();
+
+    console.log("LICITAÇÕES ENCONTRADAS:");
+
+    console.log(data);
+
+  }catch(error){
+
+    console.log("ERRO:");
+
+    console.log(error);
+
+  }
+
+}
+
+buscarLicitacoes();
