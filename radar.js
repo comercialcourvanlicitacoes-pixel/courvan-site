8520c83a457cb9ca7ec0d94c0bcd7f0ff327beaa
comercialcourvanlicitacoes-pixel <comercial.courvanlicitacoes@gmail.com>
2026-05-14 14:40:37 -0300

    Update radar.js

--- radar.js
+++ radar.js
@@ -3,7 +3,7 @@ async function buscarLicitacoes(){
   try{
 
     const response = await fetch(
-      "https://pncp.gov.br/api/consulta/v1/contratacoes/publicacao?pagina=1&tamanhoPagina=20"
+      "https://pncp.gov.br/api/consulta/v1/contratacoes/publicacao?dataInicial=2026-05-01&dataFinal=2026-05-14&pagina=1&tamanhoPagina=20"
     );
 
     const data = await response.json();
